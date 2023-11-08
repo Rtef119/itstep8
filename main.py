52efba14dce226126d1374f834fa8a169e9f8c97
rtef119 <rtef119@gmail.com>
2023-11-08 16:03:55 +0100

    itstep8

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import logging
 
 
-logging.basicConfig(level=logging.DEBUG, filename="Logs.log", filemode="w")
+logging.basicConfig(level=logging.DEBUG, filename="Logs.log", filemode="w",
+                    format="%(ascime)s: %(levelname)s -- %(message)s")
+"""
 logging.debug("Debug")
 logging.info("Info")
 logging.warning("Warning")
 logging.error("Error")
 logging.critical("Critikal")
-
+"""
 
 
 
